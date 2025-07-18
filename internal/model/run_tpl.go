ee8dd45d74757c50e396656f477fcca60374d6a2
baisiyi <2455138283@qq.com>
2025-07-18 14:00:08 +0800

    feat:build

--- internal/model/run_tpl.go
+++ internal/model/run_tpl.go
@@ -8,29 +8,10 @@ cd {{ .DestPath }}
 echo "[run.sh] chmod +x {{ .App }}"
 chmod +x {{ .App }}
 
-# 优雅地杀死占用调试端口的进程
 debugPid=$(lsof -t -i:{{ .ServerDebugPort }})
 if [ -n "$debugPid" ]; then
     echo "[run.sh] kill $debugPid"
     kill $debugPid
-    sleep 2
-    # 如果还在，强制杀死
-    if kill -0 $debugPid 2>/dev/null; then
-        echo "[run.sh] kill -9 $debugPid"
-        kill -9 $debugPid
-    fi
-fi
-
-# 优雅地杀死服务进程
-appPid=$(pgrep -f "{{ .App }}")
-if [ -n "$appPid" ]; then
-    echo "[run.sh] kill $appPid"
-    kill $appPid
-    sleep 2
-    if kill -0 $appPid 2>/dev/null; then
-        echo "[run.sh] kill -9 $appPid"
-        kill -9 $appPid
-    fi
 fi
 
 sleep 2
